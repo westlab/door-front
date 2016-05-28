489ea941c484f38fe23294b1d6fdc73b5adde7be
Ken Takagiwa <ugw.gi.world@gmail.com>
2016-05-29 08:48:08 +0900

    add src ip count #8 (#24)

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,9 @@
 import {HomeComponent} from './home/home.component';
 import {DrDomainRankComponent} from './dr-domain-rank/dr-domain-rank.component';
+import {DrSrcIpRankComponent} from './dr-src-ip-rank/dr-src-ip-rank.component';
 
 export var APP_ROUTES: any[] = [
     { path: '/', name: 'Home', component: HomeComponent },
     { path: '/domainrank', name: 'DomainRanking', component: DrDomainRankComponent },
+    { path: '/srciprank', name: 'SrcIpRanking', component: DrSrcIpRankComponent },
 ];
