496a51a49e00f41eaf9114ee56701cfa6a6085cd
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-29 22:16:55 +0900

    make mock module to suppose data from api

---
+++ app/cluster/mock-clusters.ts
@@ -0,0 +1,19 @@
+import {Cluster} from './cluster.model';
+export var CLUSTERS: Cluster[] = [
+    {
+        name: 'b4',
+        data: [
+            {name: 'foo', value: 0.06},
+            {name: 'bar', value: 0.03},
+            {name: 'baz', value: 0.02}
+        ]
+    },
+    {
+        name: 'cluster2',
+        data: [
+            {name: 'foo', value: 0.06},
+            {name: 'bar', value: 0.03},
+            {name: 'baz', value: 0.02}
+        ]
+    }
+];
