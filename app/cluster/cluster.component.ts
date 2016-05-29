ac95e9f25c0a148e307984b9ee39a762f87ee88a
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-29 13:56:34 +0900

    define cluster component and view template file

---
+++ app/cluster/cluster.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {CORE_DIRECTIVES} from '@angular/common';
+
+@Component({
+    selector: 'as-cluster',
+    templateUrl: 'app/cluster/cluster.html',
+    directives: [CORE_DIRECTIVES]
+})
+
+export class ClusterComponent {
+}
