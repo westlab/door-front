b16677dbea33c67795ae46d8dcc5f9bfabd170c4
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-30 02:14:15 +0900

    make coordinate model to avoid write coordinate in each model

--- app/cluster/cluster.model.ts
+++ app/cluster/cluster.model.ts
@@ -1,5 +1,14 @@
 import {Word} from './word.model';
+import {Coordinate} from './coordinate.model';
+
 export class Cluster {
     public name: string;
     public data: Word[];
+    public coordinate: Coordinate;
+    
+    constructor(name: string, data = [], coordinate: Coordinate) {
+        this.name = name;
+        this.data = data;
+        this.coordinate = coordinate;
+    }
 }
--- app/cluster/word.model.ts
+++ app/cluster/word.model.ts
@@ -1,10 +1,13 @@
-interface Coordinate {
-    x?: number;
-    y?: number;
-    z?: number;
-}
+import {Coordinate} from './coordinate.model';
 
-export class Word implements Coordinate {
+export class Word { 
     public name: string;
     public value: number;
+    public coordinate: Coordinate;
+    
+    constructor(name: string, value: number, coordinate: Coordinate) {
+        this.name = name;
+        this.value = value;
+        this.coordinate = coordinate;
+    }
 }
