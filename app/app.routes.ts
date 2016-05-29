9544a6c116ae975433feb61d43c3f9f533f930d9
Ken Takagiwa <ugw.gi.world@gmail.com>
2016-05-29 19:52:13 +0900

    add browsing timeline #6 (#27)

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,9 +1,11 @@
 import {HomeComponent} from './home/home.component';
 import {DrDomainRankComponent} from './dr-domain-rank/dr-domain-rank.component';
 import {DrSrcIpRankComponent} from './dr-src-ip-rank/dr-src-ip-rank.component';
+import {DrBrowsingTimelineComponent} from './dr-browsing-timeline/dr-browsing-timeline.component';
 
 export var APP_ROUTES: any[] = [
     { path: '/', name: 'Home', component: HomeComponent },
     { path: '/domainrank', name: 'DomainRanking', component: DrDomainRankComponent },
     { path: '/srciprank', name: 'SrcIpRanking', component: DrSrcIpRankComponent },
+    { path: '/timeline', name: 'Timeline', component: DrBrowsingTimelineComponent },
 ];
---
+++ app/shared/models/browsing.model.ts
@@ -0,0 +1,13 @@
+export class Browsing {
+    src_ip: string;
+    dst_ip: string;
+    src_port: number;
+    dst_port: number;
+    timestamp: string;
+    created_at: string;
+    download: number;
+    browsing_time: number;
+    title: string;
+    url: string;
+    domain: string;
+}
