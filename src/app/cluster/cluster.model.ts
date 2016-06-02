09277674bf8d093cb57224d80b06cf95a805046c
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-06-02 12:09:48 +0900

    add color property to cluster

--- src/app/cluster/cluster.model.ts
+++ src/app/cluster/cluster.model.ts
@@ -4,10 +4,12 @@ import {Coordinate} from './coordinate.model';
 export class Cluster {
     public name: string;
     public data: Word[];
+    public color: string;
     public coordinate: Coordinate;
-    constructor(name: string, data = [], coordinate: Coordinate) {
+    constructor(name: string, color: string, data = [], coordinate: Coordinate) {
         this.name = name;
         this.data = data;
+        this.color = color;
         this.coordinate = coordinate;
     }
 }
