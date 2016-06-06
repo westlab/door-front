e0bd73f4e273582b2b587c83398538de188ca4d8
giwa <ugw.gi.world@gmail.com>
2016-06-06 17:55:37 +0900

    door service #38

--- src/app/shared/api/door-service.ts
+++ src/app/shared/api/door-service.ts
@@ -3,20 +3,37 @@ import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {DomainCount} from '../models/domain-count.model';
+import {Configuration} from '../configuration';
 
 @Injectable()
 export class DoorService {
     private headers: Headers;
 
-    constructor(private _http: Http) {
-        console.log('construcotr in Door Service');
+    constructor(private _http: Http, private conf: Configuration) {
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
         this.headers.append('Accept', 'application/json');
     }
 
     public FetchDomainRank = (): Observable<DomainCount[]> => {
-        return this._http.get('http://localhost:2424/v1/domain_rank')
-                .map(res => res.json());
+        return this._http.get(this.createURL('/domain_rank'), {
+            headers: this.headers
+        }).map(res => res.json());
+    }
+
+    public FetchIPRank = (): Observable<DomainCount[]> => {
+        return this._http.get(this.createURL('/ip_rank'), {
+            headers: this.headers
+        }).map(res => res.json());
+    }
+
+    public FetchWordRank = (): Observable<DomainCount[]> => {
+        return this._http.get(this.createURL('/word_rank'), {
+            headers: this.headers
+        }).map(res => res.json());
+    }
+
+    private createURL(uri: string): string{
+       return this.conf + uri;
     }
 }
